7acdddcbd38a09c4746c916272f8338995a71e2c
Santosh Jadhav <ankushjadhavdotnetcore2023@gmail.com>
2023-08-15 00:59:21 +0530

    Added Api

---
+++ WebApiDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult GetAll(int id, string Name, Category cat)
+        {
+            Category category = new Category()
+            {
+                Id=cat.Id,
+                Name = cat.Name,
+                Description= cat.Description,
+            };
+            return Ok(category);
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebApiDemo/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiDemo.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
